36afa245843c94f195373ee776df5192cf08deb6
4444david <didkodos@gmail.com>
2025-11-16 15:36:50 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -5,14 +5,14 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
     def test_multiply(self): # 3 assertions
-        self.assertEqual(multiply(2, 3), 6)
-        self.assertEqual(multiply(-1, 5), -5)
-        self.assertEqual(multiply(0, 100), 0)
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-1, 5), -5)
+        self.assertEqual(mul(0, 100), 0)
 
     def test_divide(self): # 3 assertions
-        self.assertEqual(divide(6, 2), 3)
-        self.assertEqual(divide(-9, 3), -3)
-        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(div(6, 2), 3)
+        self.assertEqual(div(-9, 3), -3)
+        self.assertEqual(div(10, 2), 5)
 
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
@@ -34,13 +34,13 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0,0), 0)
 
     def test_subtract(self):
-        self.assertEqual(sub(12, 5),7)
-        self.assertEqual(sub(-10, -2), -8)
-        self.assertEqual(sub(0, 5), -5)
+        self.assertEqual(subtract(12, 5),7)
+        self.assertEqual(subtract(-10, -2), -8)
+        self.assertEqual(subtract(0, 5), -5)
 
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
-            div(0, 5)
+            div(5, 0)
 
 
     def test_logarithm(self): # 3 assertions
@@ -50,7 +50,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_log_invalid_base(self):
          with self.assertRaises(ValueError):
-            log(0, 3)
+            logarithm(0, 3)
 
 # Do not touch this
 if __name__ == "__main__":
